0f68f6b938cfa9865222e9a8be85a64213de58aa
中投靓仔 <453493671@qq.com>
2019-10-16 01:50:16 +0800

    add

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: 'linux-deploy-server',
-    script: 'app2.js',
+    script: 'app.js',
 
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
