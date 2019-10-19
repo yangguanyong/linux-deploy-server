a8fed3b0d2f70ad58a812c556df1db40d51626be
中投靓仔 <453493671@qq.com>
2019-10-19 09:47:26 +0800

    add mysql

--- app.js
+++ app.js
@@ -3,8 +3,35 @@ const app = new Koa();
 var cors = require('koa-cors');
 app.use(cors())
 
-app.use(async ctx => {
-  ctx.body = 'Hello World By Pm2 !!';
-});
+var Router = require('koa-router')
+var router = new Router();
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'Ygy5583330',
+  database : 'test'
+})
+
+connection.connect();
+
+router.get('/search', function (ctx, next) {
+  connection.query('SELECT * FROM test_tbl', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+  });
+})
+
+router.get('/insert', function (ctx, next) {
+  connection.query('INSERT INTO test_tbl (title) values ("李少凡")', function (error, results, fields) {
+    if (error) throw error;
+    console.log('插入成功');
+  })
+})
+
+app
+.use(router.routes())
+.use(router.allowedMethods());
 
 app.listen(3000);
