ee8c1c22b1193a87784c363bc2d0bf6ce3b2046b
中投靓仔 <453493671@qq.com>
2019-10-21 08:02:45 +0800

    add utf8

--- router/index.js
+++ router/index.js
@@ -9,7 +9,7 @@ router.get('/search', async function (ctx, next) {
 })
 
 router.get('/insert', async function (ctx, next) {
-  var result = await User.create({ firstName: "李", lastName: "少凡" });
+  var result = await User.create({ firstName: "li", lastName: "shaofan" });
   ctx.body = '创建成功' + JSON.stringify(result);
 })
 
